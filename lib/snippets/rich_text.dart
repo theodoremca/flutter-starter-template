3719a2e2d1eaaddf6254a656fb5209cfd2b71c50
theodore imonigie <35204506+theodoremca@users.noreply.github.com>
2022-02-28 21:18:13 +0100

    snippets

---
+++ lib/snippets/rich_text.dart
@@ -0,0 +1,36 @@
+//    Expanded(
+//                 child: RichText(
+//                   textAlign: TextAlign.start,
+//                   text: TextSpan(
+//                     text: 'I agree to the',
+//                     style: GoogleFonts.rubik(
+//                       color: AppColors.textBlack,
+//                       fontSize: 15.sp,
+//                     ),
+//                     children: [
+//                       TextSpan(
+//                           text: ' Terms of Use ',
+//                           style: GoogleFonts.rubik(
+//                             color: AppColors.green,
+//                             fontSize: 15.sp,
+//                             fontWeight: FontWeight.w500,
+//                           ),
+//                           recognizer: TapGestureRecognizer()..onTap = () {}),
+//                       TextSpan(
+//                         text: 'and ',
+//                         style: GoogleFonts.rubik(
+//                           color: AppColors.textBlack,
+//                           fontSize: 15.sp,
+//                         ),
+//                       ),
+//                       TextSpan(
+//                           text: ' Privacy Policy',
+//                           style: GoogleFonts.rubik(
+//                             color: AppColors.green,
+//                             fontSize: 15.sp,
+//                             fontWeight: FontWeight.w500,
+//                           ),
+//                           recognizer: TapGestureRecognizer()..onTap = () {}),
+//                     ],
+//                   ),
+//                 ))
