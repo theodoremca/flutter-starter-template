8147c40c01a080d4e89ebc4c11ccbc11e35b5d7b
theodore imonigie <35204506+theodoremca@users.noreply.github.com>
2022-07-27 19:37:28 +0100

    counter controller

--- lib/snippets/widget_usuage/countdown.dart
+++ lib/snippets/widget_usuage/countdown.dart
@@ -5,13 +5,15 @@ import 'package:timer_count_down/timer_count_down.dart';
 class CountdownWidget extends StatefulWidget {
   final Widget Function(double) child;
   final Widget Function(CountdownController controller) endChild;
-  final int  seconds;
+  final int seconds;
   final Function? onFinished;
+  final CountdownController? countdownController;
 
   const CountdownWidget({
     required this.child,
     required this.endChild,
     required this.seconds,
+    this.countdownController,
     this.onFinished,
     Key? key,
   }) : super(key: key);
@@ -22,15 +24,16 @@ class CountdownWidget extends StatefulWidget {
 
 class _CountdownWidgetState extends State<CountdownWidget> {
   final CountdownController _countdownController =
-  CountdownController(autoStart: true);
+      CountdownController(autoStart: true);
 
   @override
   Widget build(BuildContext context) {
     return Countdown(
       seconds: widget.seconds,
-      controller: _countdownController,
-      build: (BuildContext context, double time) =>
-      time < 1 ? widget.endChild(_countdownController) : widget.child(time),
+      controller: widget.countdownController ?? _countdownController,
+      build: (BuildContext context, double time) => time < 1
+          ? widget.endChild(widget.countdownController ?? _countdownController)
+          : widget.child(time),
       interval: const Duration(seconds: 1),
       onFinished: widget.onFinished,
     );
