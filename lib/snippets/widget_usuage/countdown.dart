0491c7d19784f5d74adb0cc119cc7067eba19d8b
theodore imonigie <35204506+theodoremca@users.noreply.github.com>
2022-07-27 14:36:21 +0100

    counter

---
+++ lib/snippets/widget_usuage/countdown.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:timer_count_down/timer_controller.dart';
+import 'package:timer_count_down/timer_count_down.dart';
+
+class CountdownWidget extends StatefulWidget {
+  final Widget Function(double) child;
+  final Widget Function(CountdownController controller) endChild;
+  final int  seconds;
+  final Function? onFinished;
+
+  const CountdownWidget({
+    required this.child,
+    required this.endChild,
+    required this.seconds,
+    this.onFinished,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CountdownWidget> createState() => _CountdownWidgetState();
+}
+
+class _CountdownWidgetState extends State<CountdownWidget> {
+  final CountdownController _countdownController =
+  CountdownController(autoStart: true);
+
+  @override
+  Widget build(BuildContext context) {
+    return Countdown(
+      seconds: widget.seconds,
+      controller: _countdownController,
+      build: (BuildContext context, double time) =>
+      time < 1 ? widget.endChild(_countdownController) : widget.child(time),
+      interval: const Duration(seconds: 1),
+      onFinished: widget.onFinished,
+    );
+  }
+}
