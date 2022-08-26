dab1202add25a6a33a9a0d051990f4820b9b16ee
theodore imonigie <35204506+theodoremca@users.noreply.github.com>
2022-08-26 16:43:47 +0100

    guage

---
+++ lib/snippets/guages.dart
@@ -0,0 +1,51 @@
+        //  SizedBox(
+        //         width: 100.h,
+        //         height: 100.h,
+        //         child: SfRadialGauge(
+        //           axes: <RadialAxis>[
+        //             RadialAxis(
+        //                 showLabels: false,
+        //                 showTicks: false,
+        //                 startAngle: 270,
+        //                 endAngle: 270,
+        //                 radiusFactor: 0.3,
+        //                 axisLineStyle: const AxisLineStyle(
+        //                     thicknessUnit: GaugeSizeUnit.factor,
+        //                     thickness: 0.15),
+        //                 annotations: <GaugeAnnotation>[
+        //                   GaugeAnnotation(
+        //                       angle: 180,
+        //                       widget: Row(
+        //                         mainAxisSize: MainAxisSize.min,
+        //                         children: const <Widget>[
+        //                           Text(
+        //                             '50%',
+        //                             style: TextStyle(
+        //                                 fontFamily: 'Times',
+        //                                 fontSize: 8,
+        //                                 fontWeight: FontWeight.w400,
+        //                                 fontStyle: FontStyle.italic),
+        //                           ),
+        //                         ],
+        //                       )),
+        //                 ],
+        //                 pointers: const <GaugePointer>[
+        //                   RangePointer(
+        //                       value: 50,
+        //                       cornerStyle: CornerStyle.bothCurve,
+        //                       enableAnimation: true,
+        //                       animationDuration: 1200,
+        //                       sizeUnit: GaugeSizeUnit.factor,
+        //                       gradient: SweepGradient(colors: <Color>[
+        //                         Color(0xFF6A6EF6),
+        //                         Color(0xFFDB82F5)
+        //                       ],
+        //                           stops: <double>[
+        //                         0.25,
+        //                         0.75
+        //                       ]),
+        //                       color: Color(0xFF00A8B5),
+        //                       width: 0.15),
+        //                 ]),
+        //           ],
+        //         )),
